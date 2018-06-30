91e1313cc2f0b632dd7bb5f72db9af5490815612
rso16 <rick.overhorst@gmail.com>
2018-06-30 14:38:43 +0200

    added readI2CData added sendI2CReStart changed UARTInit (baud is now a float instead of uint32_t)

--- Microcontroller.h
+++ Microcontroller.h
@@ -13,8 +13,8 @@
 #define EVENPARITY  1
 #define ODDPARITY   2
 
-#define NORMALSPEED 0
-#define DOUBLESPEED 1
+#define DOUBLESPEED 0
+#define NORMALSPEED 1
 
 // #define INPUT       0
 // #define OUTPUT      1
@@ -40,8 +40,9 @@ class Microcontroller
      virtual void    sendI2CStop()             = 0;
      virtual void    sendI2CAddr(uint8_t addr) = 0;
      virtual void    sendI2CData(uint8_t data) = 0;
-
-     virtual void    UARTInit(uint8_t dataBits, uint8_t parityBit, uint8_t stopBits, uint32_t baud, uint8_t speed, uint8_t RT) = 0; //fuction to initialize the UART, takes the amount of data bits (5,6,6,8 or 9 bits), no, even or an odd parity bit, the amound of stop bits (1 or 2) and the baudrate returns the status code
+     virtual uint8_t readI2CData()             = 0;
+     virtual void    sendI2CReStart()          = 0;
+     virtual void    UARTInit(uint8_t dataBits, uint8_t parityBit, uint8_t stopBits, float baud, uint8_t speed, uint8_t RT) = 0; //fuction to initialize the UART, takes the amount of data bits (5,6,6,8 or 9 bits), no, even or an odd parity bit, the amound of stop bits (1 or 2) and the baudrate returns the status code
      virtual void    UARTBegin(long int baud)   = 0; //function to begin the UART with the defaul settings and a baudrate. returns an status code.
      virtual void    UARTSend(uint16_t data)    = 0;
      virtual void    UARTSend(uint8_t data)     = 0;
